4dcc0eab0fcf458410ffff2b158196740d140554
Ahad726 <atmahad726@gmail.com>
2021-03-19 18:41:49 +0600

    user and role added

---
+++ WebAPICore5/WebAPI.Core/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPI.Core
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string  Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string passwordHash { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
