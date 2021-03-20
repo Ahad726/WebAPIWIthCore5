715f0d2b24f17f6b2827ea7da8af93ead2c5e5a2
Ahad726 <atmahad726@gmail.com>
2021-03-20 06:47:13 +0600

    account controller added

---
+++ WebAPICore5/WebAPICore5/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICore5.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+
+        }
+    }
+}
