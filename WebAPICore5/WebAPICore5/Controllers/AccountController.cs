e69a9f2dd380b08b9fd51069d6919ed77923348e
Ahad726 <atmahad726@gmail.com>
2021-03-20 07:36:10 +0600

    account>register method added

--- WebAPICore5/WebAPICore5/Controllers/AccountController.cs
+++ WebAPICore5/WebAPICore5/Controllers/AccountController.cs
@@ -4,16 +4,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Core;
+using WebAPI.Store.IServices;
+using WebAPICore5.Models;
 
 namespace WebAPICore5.Controllers
 {
     [Route("api/account")]
     public class AccountController : ControllerBase
     {
-        [HttpPost("register")]
-        public IActionResult Register()
-        {
+        private readonly IUserService userService;
 
+        public AccountController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+
+            var newUser = new User()
+            {
+                Email = model.Email,
+                passwordHash = model.password,
+                DateOfBirth = model.DateOfBirth,
+                RoleId = model.RoleId
+            };
+
+            userService.Register(newUser);
+
+            return Ok();
         }
     }
 }
